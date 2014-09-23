a307e407f2477cebafe15f659e0a366b04591649
Brice Fernandes <brice.fernandes@gmail.com>
2014-09-23 18:45:11 +0100

    C08 - How To Load Scenes and Quit

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -5,10 +5,12 @@ public class LevelManager : MonoBehaviour {
 
 	public void LoadLevel(string name){
 		Debug.Log ("New Level load: " + name);
+		Application.LoadLevel (name);
 	}
 
 	public void QuitRequest(){
 		Debug.Log ("Quit requested");
+		Application.Quit ();
 	}
 
 }
