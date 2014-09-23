ef393e46fae30284cf059a875fb300b8e4b7c1ad
Brice Fernandes <brice.fernandes@gmail.com>
2014-09-23 15:15:38 +0100

    C06 - Calling Scripts from Buttons

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+		Debug.Log ("New Level load: " + name);
+	}
+
+	public void QuitRequest(){
+		Debug.Log ("Quit requested");
+	}
+
+}
